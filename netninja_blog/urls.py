de92ddd1591475208c056d4bcbcae394a7bd6480
kinganuoluwa <adekogbonking03@gmail.com>
2022-10-20 11:40:18 +0100

    created aricles app

--- netninja_blog/urls.py
+++ netninja_blog/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', views.home),
-    path('about/', views.about)
+    path('about/', views.about),
+    path('articles/', include('articles.urls'))
 ]
