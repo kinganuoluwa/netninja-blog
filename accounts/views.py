46135b9b372993ec9d4e50711a1b6341051cff1b
kinganuoluwa <adekogbonking03@gmail.com>
2022-10-25 07:09:27 +0100

    setup homepage

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView, LogoutView
+from django.shortcuts import render, redirect
+
 
 def signup(request):
     if request.method == 'POST':
--- netninja_blog/urls.py
+++ netninja_blog/urls.py
@@ -1,14 +1,15 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
 from . import views
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.conf.urls.static import static
-from django.conf import settings
+from articles.views import article_list
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', views.home),
+    path('', article_list),
     path('about/', views.about),
     path('articles/', include('articles.urls')),
     path('accounts/', include('accounts.urls')),
