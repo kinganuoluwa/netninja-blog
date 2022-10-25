58683aa660fd1f7ee18a11f90091e42341ef88fb
kinganuoluwa <adekogbonking03@gmail.com>
2022-10-25 07:32:27 +0100

    removed about page

--- netninja_blog/urls.py
+++ netninja_blog/urls.py
@@ -3,14 +3,12 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
-from . import views
 from articles.views import article_list
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', article_list),
-    path('about/', views.about),
     path('articles/', include('articles.urls')),
     path('accounts/', include('accounts.urls')),
 ]
