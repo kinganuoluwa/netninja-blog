f150410b0c06c4d0b2027f617a06ecd722e006fc
kinganuoluwa <adekogbonking03@gmail.com>
2022-10-25 02:35:17 +0100

    setup static files

--- netninja_blog/urls.py
+++ netninja_blog/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
@@ -9,3 +10,5 @@ urlpatterns = [
     path('about/', views.about),
     path('articles/', include('articles.urls'))
 ]
+
+urlpatterns += staticfiles_urlpatterns()
