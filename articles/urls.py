f16e54aa00df359b3baf4036597ff1600334462e
kinganuoluwa <adekogbonking03@gmail.com>
2022-10-25 03:09:32 +0100

    added articles detail

--- articles/urls.py
+++ articles/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.article_list),
+    path('', views.article_list, name='article_list'),
+    path('<slug>/', views.article_detail, name='article_detail'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -5,3 +5,7 @@ from .models import Article
 def article_list(request):
     articles = Article.objects.all()
     return render(request, 'articles/article_list.html', {'articles':articles})
+
+def article_detail(request, slug):
+    article = Article.objects.get(slug=slug)
+    return render(request, 'articles/article_detail.html', {'article':article})
